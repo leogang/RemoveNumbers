e6f5fd6bb4f6ab35105b66d5d134b71f65b5cc99
Cody Albert <leogang89@gmail.com>
2014-07-07 17:10:12 -0700

    Edited the comments for style

--- RenameFiles.py
+++ RenameFiles.py
@@ -1,14 +1,14 @@
 import os
 
 def rename_files():
-    #(1) get file names from a folder
+    #Get file names from a folder
     file_list = os.listdir("/Users/animaltalk/Downloads/prank")
     print file_list
     saved_path = os.getcwd()
     print "Current Working directory is " + saved_path
     os.chdir(r"/Users/animaltalk/Downloads/prank")
     
-    #(2) for each file, rename filename
+    #For each file, rename filename
     for file_name in file_list:
             print "Old Name - " + file_name
             os.rename(file_name, file_name.translate(None, "0123456789"))
